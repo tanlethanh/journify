bf47a26fc652600c4c198819d2bf015268800a48
Tan Le <tanlethanh8266@gmail.com>
2023-11-27 06:31:31 +0700

    update landing

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Journify - coming soon</h1>
+      <h1 className="text-3xl">Journify - coming soon</h1>
     </main>
   )
 }
