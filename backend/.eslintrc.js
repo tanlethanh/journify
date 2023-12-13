2df924ea6891d252a90a7310402cf7f9039842b6
Tan Le <tanlethanh8266@gmail.com>
2023-12-13 09:21:58 +0700

    chore: reconfig be

---
+++ backend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    '../.eslintrc.json',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
