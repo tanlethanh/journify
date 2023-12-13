d74e490bad4a118ee0ca98cc8d200aeecf0f5696
Tan Le <tanlethanh8266@gmail.com>
2023-12-13 19:13:34 +0700

    feat: config swagger with nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
 	imports: [UsersModule, PostsModule],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Journify API')
+		.setDescription(
+			'RESTful API for Journify - A trusted, memorable, and enjoyable travel experience mobile application',
+		)
+		.setVersion('1.0')
+		.addTag('Journify')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(8080);
 }
 bootstrap();
