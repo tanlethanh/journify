491c9af2afa1d03c90d0102bae6ad28e60c09ca5
Tan Le <tanlethanh8266@gmail.com>
2023-12-23 15:38:09 +0700

    feat: sign flow

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,136 @@
+import { useEffect } from 'react';
+import {
+	Image,
+	Linking,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { signInWithGoogle, useAuth } from '../utils';
+
+const SignIn = () => {
+	const { user } = useAuth();
+	const navigation = useNavigation();
+
+	const handlePressContact = () => {
+		Linking.openURL('https://journify.info');
+	};
+
+	const handleSignInWithGoogle = () => {
+		signInWithGoogle();
+	};
+
+	useEffect(() => {
+		if (user) navigation.navigate('Map' as never);
+	}, [user]);
+
+	return (
+		<View style={styles.container}>
+			<StatusBar
+				translucent
+				backgroundColor={'transparent'}
+				barStyle={'dark-content'}
+			/>
+			<View style={styles.signInContainer}>
+				<Text style={styles.titleText}>Sign In</Text>
+
+				<View style={styles.signOptions}>
+					<TouchableOpacity style={styles.signInButton}>
+						<Image
+							style={styles.logoSize}
+							source={require('@/assets/images/facebook_logo.png')}
+						/>
+						<Text style={styles.signInText}>Sign in with Facebook</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.signInButton}
+						onPress={handleSignInWithGoogle}
+					>
+						<Image
+							style={styles.logoSize}
+							source={require('@/assets/images/google_logo.png')}
+						/>
+						<Text style={styles.signInText}>Sign in with Google</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+
+			<View style={styles.nameContainer}>
+				<Text style={styles.nameText}>Journify</Text>
+			</View>
+
+			<View style={styles.helpContainer}>
+				<Text>{"Can't sign in?"}</Text>
+				<TouchableOpacity onPress={handlePressContact}>
+					<Text style={styles.contactText}>Contact us</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+	},
+	nameContainer: {
+		position: 'absolute',
+		top: 140,
+		left: 0,
+		right: 0,
+		alignItems: 'center',
+	},
+	nameText: {
+		fontSize: 24,
+		fontFamily: 'OldStandardTT-Bold',
+		color: '#000000',
+	},
+	signInContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		gap: 60,
+	},
+	titleText: {
+		fontSize: 32,
+		fontWeight: '700',
+		alignSelf: 'center',
+		color: '#000000',
+	},
+	signOptions: {
+		gap: 10,
+	},
+	signInButton: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 32,
+		padding: 24,
+		borderWidth: 1,
+		borderRadius: 50,
+		borderColor: '#D1D1D1',
+	},
+	signInText: {
+		color: '#000000',
+	},
+	logoSize: {
+		width: 28,
+		height: 28,
+	},
+	helpContainer: {
+		bottom: 20,
+		left: 0,
+		right: 0,
+		flexDirection: 'row',
+		gap: 4,
+		justifyContent: 'center',
+	},
+	contactText: {
+		color: '#1F41F4',
+	},
+});
--- mobile/src/screens/Splash.tsx
+++ mobile/src/screens/Splash.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ImageBackground, StatusBar, StyleSheet, View } from 'react-native';
-import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated';
+import Animated, { FadeInDown } from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../utils';
 
 const Splash = () => {
 	const navigation = useNavigation();
+	const { initializing, user } = useAuth();
+	const [waiting, setWaiting] = useState(true);
 
 	useEffect(() => {
-		setTimeout(() => {
-			navigation.navigate('FirstOnBoarding' as never);
-		}, 3500);
+		if (waiting || initializing) return;
+		if (user) {
+			navigation.navigate('Map' as never);
+		} else {
+			navigation.navigate('SignIn' as never);
+		}
+	}, [initializing, user, waiting]);
+
+	useEffect(() => {
+		const timer = setTimeout(() => setWaiting(false), 1000);
+
+		return () => {
+			clearTimeout(timer);
+		};
 	}, []);
 
 	return (
@@ -20,23 +35,20 @@ const Splash = () => {
 				source={require('@/assets/images/vietnam-road.png')}
 			/>
 
-			<Animated.View
-				style={styles.contentContainer}
-				entering={FadeIn.delay(500).duration(800)}
-			>
+			<View style={styles.contentContainer}>
 				<Animated.Text
 					style={styles.appName}
-					entering={FadeInDown.delay(800).duration(1000)}
+					entering={FadeInDown.duration(1000)}
 				>
 					Journify
 				</Animated.Text>
 				<Animated.Text
 					style={styles.slogan}
-					entering={FadeInDown.delay(1300).duration(1000)}
+					entering={FadeInDown.delay(1000).duration(1000)}
 				>
 					Life is a journey, enjoy the ride
 				</Animated.Text>
-			</Animated.View>
+			</View>
 		</View>
 	);
 };
@@ -48,12 +60,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	backgroundImage: {
+		flex: 1,
 		resizeMode: 'cover',
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
 	},
 	contentContainer: {
 		position: 'absolute',
