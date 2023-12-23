bb3151c56a6e8b9fba5058b62504e3e7818fc7c6
Tan Le <tanlethanh8266@gmail.com>
2023-12-23 12:32:52 +0700

    feat: use current location

--- mobile/src/screens/Map/utils.ts
+++ mobile/src/screens/Map/utils.ts
@@ -1,5 +1,57 @@
+import { useEffect, useState } from 'react';
 import { PermissionsAndroid, Platform } from 'react-native';
 import Geo from 'react-native-geolocation-service';
+import type { Region } from 'react-native-maps';
+
+// Ho Chi Minh city
+const defaultLocation: Region = {
+	latitude: 10.762622,
+	latitudeDelta: 0.04,
+	longitude: 106.660172,
+	longitudeDelta: 0.02,
+};
+
+export const useLocation = () => {
+	const [region, setRegion] = useState<Region>(defaultLocation);
+
+	const resolveLocation = async () => {
+		const granted = await grantLocationPermission();
+		if (!granted) {
+			console.log('not granted');
+			return;
+		}
+
+		try {
+			const loc = await getCurrentLocation();
+			console.log(loc, '<-- location');
+			setRegion({
+				latitude: loc.coords.latitude as number,
+				latitudeDelta: defaultLocation.latitudeDelta,
+				longitude: loc.coords.longitude as number,
+				longitudeDelta: defaultLocation.longitudeDelta,
+			});
+		} catch (e) {
+			console.log('can not get current location', e);
+		}
+	};
+
+	useEffect(() => {
+		resolveLocation();
+
+		const watchId = Geo.watchPosition((loc) => {
+			setRegion({
+				latitude: loc.coords.altitude as number,
+				latitudeDelta: defaultLocation.latitudeDelta,
+				longitude: loc.coords.longitude as number,
+				longitudeDelta: defaultLocation.longitudeDelta,
+			});
+		});
+
+		return Geo.clearWatch(watchId);
+	}, []);
+
+	return { region };
+};
 
 export const grantLocationPermission = async (): Promise<boolean> => {
 	if (Platform.OS === 'android') {
@@ -13,4 +65,13 @@ export const grantLocationPermission = async (): Promise<boolean> => {
 	}
 
 	return false;
+};
+
+const getCurrentLocation = (): Promise<Geo.GeoPosition> => {
+	return new Promise((resolve, reject) => {
+		Geo.getCurrentPosition(
+			(pos) => resolve(pos),
+			(err) => reject(err),
+		);
+	});
 };
