a0c023d490441b40346ba611f3742257049cd77a
Tan Le <tanlethanh8266@gmail.com>
2023-12-23 13:11:53 +0700

    feat: add search bar with icons

---
+++ mobile/src/screens/Map/components/Search.tsx
@@ -0,0 +1,41 @@
+import type { FC } from 'react';
+import type { ViewStyle } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { MapPinIcon, SearchIcon } from 'lucide-react-native';
+
+interface Props {
+	style?: ViewStyle;
+}
+
+const Search: FC<Props> = ({ style }) => {
+	return (
+		<View style={[style, styles.container]}>
+			<SearchIcon color="#848484" />
+			<TextInput
+				style={styles.searchInput}
+				placeholder="Type location..."
+				placeholderTextColor={'#848484'}
+			/>
+			<MapPinIcon color="#848484" />
+		</View>
+	);
+};
+
+export default Search;
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 10,
+		backgroundColor: '#FFFFFF',
+		paddingHorizontal: 14,
+		paddingVertical: 14,
+		borderRadius: 200,
+	},
+	searchInput: {
+		flex: 1,
+		color: '#000000',
+		padding: 0,
+	},
+});
--- mobile/src/screens/Map/index.tsx
+++ mobile/src/screens/Map/index.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import Search from './components/Search';
 import { useLocation } from './utils';
 
 export const Map = () => {
 	const { region } = useLocation();
+	const { top } = useSafeAreaInsets();
+	const searchBarStyle = { ...styles.searchBar, top };
 
 	return (
-		<MapView
-			style={styles.container}
-			initialRegion={region}
-			region={region}
-			showsUserLocation={true}
-		/>
+		<View style={styles.container}>
+			<MapView
+				style={styles.mapContainer}
+				initialRegion={region}
+				region={region}
+				showsUserLocation={true}
+			/>
+			<View style={searchBarStyle}>
+				<Search />
+			</View>
+		</View>
 	);
 };
 
@@ -21,6 +30,14 @@ export default Map;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'yellow',
+	},
+	searchBar: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		paddingHorizontal: 14,
+	},
+	mapContainer: {
+		flex: 1,
 	},
 });
