22677e46c8f13e7c4667a09fb243a243f4ad9363
Tan Le <tanlethanh8266@gmail.com>
2023-12-25 11:24:59 +0700

    fix: resolve project modules

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,7 +7,9 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+	watchFolders: [path.resolve(__dirname, '../node_modules')],
+};
 
 // eslint-disable-next-line no-undef
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
