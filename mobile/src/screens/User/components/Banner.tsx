c578ad14ac302311b36f1534f0f92c1bc793b4e5
Tan Le <tanlethanh8266@gmail.com>
2024-01-02 02:36:03 +0700

    feat: setting button

--- mobile/src/screens/User/components/Banner.tsx
+++ mobile/src/screens/User/components/Banner.tsx
@@ -1,6 +1,6 @@
 import { type FC, useEffect } from 'react';
 import type { ViewStyle } from 'react-native';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import Animated, {
 	interpolate,
 	useAnimatedStyle,
@@ -8,6 +8,7 @@ import Animated, {
 	withTiming,
 } from 'react-native-reanimated';
 import { useIsFocused } from '@react-navigation/native';
+import { SettingsIcon } from 'lucide-react-native';
 
 type Props = {
 	style?: ViewStyle;
@@ -36,6 +37,9 @@ export const Banner: FC<Props> = ({ style }) => {
 			<Animated.Text style={[styles.text, animatedStyle]}>
 				{'Hi Journify'}
 			</Animated.Text>
+			<TouchableOpacity style={styles.settingContainer}>
+				<SettingsIcon style={styles.settingIcon} size={16} color="white" />
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -48,10 +52,21 @@ const styles = StyleSheet.create({
 		backgroundColor: '#7e7e8a',
 		justifyContent: 'center',
 		alignItems: 'center',
+		flexDirection: 'row',
 	},
 	text: {
 		color: '#FFFFFF',
 		fontWeight: '800',
 		fontSize: 20,
+	},
+	settingContainer: {
+		position: 'absolute',
+		right: 10,
+		backgroundColor: '#0000008F',
+		padding: 6,
+		borderRadius: 20,
+	},
+	settingIcon: {
+		paddingTop: 10,
 	},
 });
