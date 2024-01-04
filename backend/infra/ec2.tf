794bf09972ac6e177e2c82da6237db4cc1118a4f
Tan Le <tanlethanh8266@gmail.com>
2024-01-04 11:52:09 +0700

    fix: update newest deployment script

--- backend/infra/ec2.tf
+++ backend/infra/ec2.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "journify" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum install -y docker",
+      "sudo yum install -y git",
       "sudo service docker start",
       "sudo usermod -a -G docker ec2-user",
       "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
